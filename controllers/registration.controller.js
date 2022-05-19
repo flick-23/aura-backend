defd32dd5d49681342007080a73099bb98ed7e22
Venkatesh D <venkigd8@gmail.com>
2022-05-19 10:58:35 +0530

    Added a check if a student is valid website user before registering for the event

--- controllers/registration.controller.js
+++ controllers/registration.controller.js
@@ -6,27 +6,41 @@ const asyncRoute =
   (req, res, next = console.error) =>
     Promise.resolve(route(req, res)).catch(next);
 
-const create = function (req, res) {
-  let teamData;
-  models.Team.create({
-    eventId: req.body.eventId,
-    name: req.body.name,
-    usn_1: req.body.usn[0],
-    usn_2: req.body.usn[1],
-    usn_3: req.body.usn[2],
-    usn_4: req.body.usn[3],
-    usn_5: req.body.usn[4],
-  })
-    .then((team) => {
-      teamData = team.dataValues.id;
+const create = async function (req, res) {
+  const user = await models.User.findAll();
+  const userUsn = user.map((u) => {
+    return u.dataValues.usn;
+  });
 
-      models.Registration.create({
-        teamId: teamData,
-        eventId: req.body.eventId,
-      });
+  const usn = req.body.usn.filter((u) => u != "");
 
-      ReS(res, team, 200);
+  const containsAll = usn.every((element) => {
+    return userUsn.indexOf(element) !== -1;
+  });
+
+  if (containsAll) {
+    models.Team.create({
+      eventId: req.body.eventId,
+      name: req.body.name,
+      usn_1: req.body.usn[0].toUpperCase(),
+      usn_2: req.body.usn[1].toUpperCase(),
+      usn_3: req.body.usn[2].toUpperCase(),
+      usn_4: req.body.usn[3].toUpperCase(),
+      usn_5: req.body.usn[4].toUpperCase(),
     })
-    .catch((err) => ReE(res, err, 422));
+      .then((team) => {
+        const teamData = team.dataValues.id;
+
+        models.Registration.create({
+          teamId: teamData,
+          eventId: req.body.eventId,
+        });
+
+        ReS(res, team, 200);
+      })
+      .catch((err) => ReE(res, err, 422));
+  } else {
+    res.send(404);
+  }
 };
 module.exports.create = create;
